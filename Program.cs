f0f645790187e4d370dcc3f21a03ff8c21b2a304
Ted Liou <mail@tedliou.com>
2023-03-16 01:16:30 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using IniParser;
+using IniParser.Model;
+
+namespace console_iniparser_example
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            var path = "config.ini";
+            var parser = new FileIniDataParser();
+
+            if (!File.Exists(path))
+            {
+                var defaultConfig = new IniData();
+                defaultConfig["size"]["width"] = "50";
+                parser.WriteFile(path, defaultConfig);
+            }
+
+            var data = parser.ReadFile(path);
+            Console.WriteLine(data["size"]["width"]);
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
